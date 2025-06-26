b53e6ded7fd9364e71d033984605d65968f3e8c0
Matheus Cirillo <matheuscirillo@usp.br>
2025-06-25 21:03:48 -0300

    reading adc but not sending it

--- transmitter/main/main.cpp
+++ transmitter/main/main.cpp
@@ -1,49 +1,66 @@
 #include <stdio.h>
 #include <nvs_flash.h>
 #include <stdbool.h>
+
+#include "string.h"
+#include "esp_system.h"
+#include "esp_sleep.h"
 #include "esp_log.h"
 #include "esp_err.h"
-#include "string.h"
 #include "esp_wifi.h"
 #include "esp_mac.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
+#include "driver/gpio.h"
 
 #include "driver/twai.h"
 
 #include "wcan_communication.h"
 #include "wcan_utils.h"
 
-uint16_t allowed_ids[3] = {0x123, 0x456, 0x789};
+uint16_t allowed_ids[16] = {0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F};
 #define ALLOWED_IDS_SIZE (sizeof(allowed_ids) / sizeof(allowed_ids[0]))
 
+#define OUTPUT_HIGH_PIN     GPIO_NUM_23
+#define CHARGING_CTRL_PIN   GPIO_NUM_22
+
 #define MAX_UINT32 0xFFFFFFFF
+
+#include "driver/adc.h"
+#define ADC_CHANNEL ADC1_CHANNEL_4  // GPIO4 corresponds to ADC1_CHANNEL_4
 
 static void ReadDataTask(void *pvParameter){
     static const char *TAG = "READ";
     ESP_LOGI(TAG, "Read data task started");
-    uint32_t val = 0;
+    int val = 0;
+
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_12);
 
     while(1) {
+        val = adc1_get_raw(ADC_CHANNEL);
+        ESP_LOGI(TAG, "[%04x] %i", 0x512, val);
+        vTaskDelay(pdMS_TO_TICKS(200));
+        continue;
+
         data_packet_t send_data;
         send_data.can_id = 0x123;
         send_data.payload = NULL;
         send_data.payload_len = 0;
 
         send_data.payload_len = sizeof(val);
-        send_data.payload = (uint8_t *)malloc(send_data.payload_len);
-        if (send_data.payload == NULL) {
-            ESP_LOGE(TAG, "Malloc payload fail");
+                send_data.payload = (uint8_t *)malloc(send_data.payload_len);
+                if (send_data.payload == NULL) {
+                    ESP_LOGE(TAG, "Malloc payload fail");
             break;
-        }
+                }
         memcpy(send_data.payload, &val, send_data.payload_len);
-        ESP_LOGI(TAG, "[%04x] %lu", send_data.can_id, val);
+        //ESP_LOGI(TAG, "[%04x] %lu", send_data.can_id, val);
 
         ESP_LOGV(TAG, "send_data.payload: %p\n", (void*)send_data.payload);
         if (xQueueSend(send_queue, &send_data, ESPNOW_MAXDELAY) == pdTRUE) {
             ESP_LOGD(TAG, "Data read successfully");
-        } else {
+                } else {
             ESP_LOGW(TAG, "Send send queue fail");
             free(send_data.payload);
         }
@@ -59,48 +76,29 @@ void RecvCallback(data_packet_t data)
     static const char *TAG = "USER-RECV";
     switch (data.can_id)
     {
-        case 0x123:{
+        case 0x51:{
             uint32_t uint_data = *(uint32_t *)(data.payload);
-            if (uint_data % 5 != 0) break;
-            
-            ESP_LOGI(TAG, "[%04x] %lu", data.can_id, uint_data);
-            // Configure message to transmit
-            twai_message_t message = {
-                .identifier = 0x123,
-                .data_length_code = data.payload_len,
-            };
-
-            memcpy(message.data, data.payload, data.payload_len);
-
-            twai_status_info_t status_info;
-            if (twai_get_status_info(&status_info) == ESP_OK) {
-                ESP_LOGI(TAG, "TWAI Status: state=%d, msgs_to_tx=%ld, msgs_to_rx=%ld, tx_err_cnt=%ld, rx_err_cnt=%ld, arb_lost_cnt=%ld, bus_err_cnt=%ld",
-                        status_info.state,
-                        status_info.msgs_to_tx,
-                        status_info.msgs_to_rx,
-                        status_info.tx_error_counter,
-                        status_info.rx_error_counter,
-                        status_info.arb_lost_count,
-                        status_info.bus_error_count);
-                if (status_info.state == TWAI_STATE_BUS_OFF) {
-                    ESP_LOGE(TAG, "TWAI controller is BUS OFF. Attempting recovery...");
-                    twai_initiate_recovery(); // Or twai_start() again after twai_stop() depending on recovery strategy
-                }
-            }
-
-            // Queue message for transmission
-            esp_err_t err = twai_transmit(&message, pdMS_TO_TICKS(1000));
-            if (err == ESP_OK) {
-                ESP_LOGI(TAG, "Message queued for transmission\n");
-            } else {
-                ESP_LOGE(TAG, "Failed to queue message for transmission: %s\n", esp_err_to_name(err));
-            }
+            ESP_LOGI(TAG, "Strain Gauge [%04x]: %lu", data.can_id, uint_data);
             break;
         }
-        default:
+        default:{
             ESP_LOGE(TAG, "[%04x] Unknown", data.can_id);
             PrintCharPacket(data.payload, data.payload_len);
             break;
+        }
+    }
+
+    twai_message_t message = {
+        .identifier = (uint32_t)data.can_id,
+        .data_length_code = data.payload_len,
+    };
+    memcpy(message.data, data.payload, data.payload_len);
+
+    esp_err_t err = twai_transmit(&message, pdMS_TO_TICKS(1000));
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "Message queued for transmission\n");
+    } else {
+        ESP_LOGE(TAG, "Failed to queue message for transmission: %s\n", esp_err_to_name(err));
     }
 }
 
@@ -144,6 +142,23 @@ static void CanInit(){
 
 extern "C" void app_main(void){
     static const char *TAG = "MAIN";
+    gpio_reset_pin(OUTPUT_HIGH_PIN);
+    gpio_set_direction(OUTPUT_HIGH_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(OUTPUT_HIGH_PIN, 1);
+
+    gpio_reset_pin(CHARGING_CTRL_PIN);
+    gpio_set_direction(CHARGING_CTRL_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(CHARGING_CTRL_PIN, GPIO_PULLDOWN_ONLY);
+
+    int pin_state = gpio_get_level(CHARGING_CTRL_PIN);
+
+    if (pin_state == 0) {
+
+        ESP_ERROR_CHECK(gpio_wakeup_enable(CHARGING_CTRL_PIN, GPIO_INTR_HIGH_LEVEL));
+        ESP_ERROR_CHECK(esp_sleep_enable_gpio_wakeup());
+        ESP_LOGI(TAG, "Going to sleep now...");
+        esp_deep_sleep_start();
+    }
     
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -164,5 +179,5 @@ extern "C" void app_main(void){
     ESP_LOGI(TAG, "MAC address: %02x:%02x:%02x:%02x:%02x:%02x", 
                 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
-    //xTaskCreate(ReadDataTask, "ReadDataTask", 4096, NULL, 5, NULL);
+    xTaskCreate(ReadDataTask, "ReadDataTask", 4096, NULL, 5, NULL);
 }
